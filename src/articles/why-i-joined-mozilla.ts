d0041839e11b3ba71984938f2ab25e5d163768d4
wuebkers <skylar@skylarwuebker.com>
2023-11-07 15:53:34 -0500

    fixes typo

--- src/articles/why-i-joined-mozilla.ts
+++ src/articles/why-i-joined-mozilla.ts
@@ -6,7 +6,7 @@ export const content: object = {
     job_title: 'Chief Legal Officer',
     image: '/public/headshot.png',
   },
-  lead_article: true,
+  lead_article: false,
   excerpt: 'Carlos Torres joined the team in August&apos;s Chief Legal Officer. ',
   body: '<p>YCarlos is responsible for leading the company’s global legal and public policy teams that develop the legal, regulatory and policy strategy to support the company’s mission to create a healthy internet. Carlos also serves as Corporate Secretary.</p><blockquote>I was drawn to Mozilla because of its unique voice in technology,” Torres said. “There is no other organization that combines community, product, technology and advocacy to produce trusted innovative products that people love. I&apos;ve always admired Mozilla for its principled, people-focused approach.</blockquote><p>Most recently, Carlos was general counsel of several venture-backed companies in the proptech, supply chain and crypto industries. Prior to that he spent over a decade at Salesforce in a variety of leadership roles. He also served as senior counsel of a biotech company and started his legal career at Orrick, Herrington & Sutcliffe.</p>'
 };
