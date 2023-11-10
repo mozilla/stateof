c86a678efa75cf5d151004a6845780d4e3b5e94e
wuebkers <skylar@skylarwuebker.com>
2023-11-10 16:46:39 -0500

    adds the first row of articles

--- src/articles/this_is_an _article_title_for_here copy 2.ts
+++ src/articles/this_is_an _article_title_for_here copy 2.ts
@@ -1,13 +1,14 @@
 export const content: object = {
-  title: 'Why I joined Mozilla 1',
-  hash: '#why-i-joined-mozilla-1',
+  title: 'This is an article title for here 2',
+  hash: '#this-is-an-article-title-for-here-2',
   author: {
     name: 'Steve Teixeira',
     job_title: 'Job Title',
-    image: '/headshot.png',
+    image: null,
   },
   lead_article: false,
   modal_style: 'short',
+  icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 573 286"><g clip-path="url(#a)"> <rect width="746" height="286" x="-186" fill="#CEC2FF" opacity=".5" rx="143" style="mix-blend-mode:multiply"/><path fill="#CEC2FF" d="M296 0h277v286H296z" style="mix-blend-mode:multiply"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h573v286H0z"/></clipPath></defs></svg>',
   excerpt: 'Steve Teixeira joined the team in August’s Chief Legal Officer.',
   body: '<blockquote>I was drawn to Mozilla because of its unique voice in technology,” Torres said. “There is no other organization that combines community, product, technology and advocacy to produce trusted innovative products that people love. I’ve always admired Mozilla for its principled, people-focused approach.</blockquote>'
 };
---
+++ src/articles/this_is_an _article_title_for_here copy.ts
@@ -0,0 +1,13 @@
+export const content: object = {
+  title: 'This is an article title for here 1',
+  hash: '#this-is-an-article-title-for-here-1',
+  author: {
+    name: 'Steve Teixeira',
+    job_title: 'Job Title',
+    image: null,
+  },
+  lead_article: false,
+  modal_style: 'short',
+  excerpt: 'Steve Teixeira joined the team in August’s Chief Legal Officer.',
+  body: '<blockquote>I was drawn to Mozilla because of its unique voice in technology,” Torres said. “There is no other organization that combines community, product, technology and advocacy to produce trusted innovative products that people love. I’ve always admired Mozilla for its principled, people-focused approach.</blockquote>'
+};
