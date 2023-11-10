ef3d0275b0d6fb4eb74ec146a3c7640668302935
wuebkers <skylar@skylarwuebker.com>
2023-11-10 12:13:48 -0500

    makes last modal layout

---
+++ src/articles/why-i-joined-mozilla-1.ts
@@ -0,0 +1,13 @@
+export const content: object = {
+  title: 'Why I joined Mozilla 1',
+  hash: '#why-i-joined-mozilla-1',
+  author: {
+    name: 'Steve Teixeira',
+    job_title: 'Job Title',
+    image: '/headshot.png',
+  },
+  lead_article: false,
+  modal_style: 'short',
+  excerpt: 'Steve Teixeira joined the team in August’s Chief Legal Officer.',
+  body: '<blockquote>I was drawn to Mozilla because of its unique voice in technology,” Torres said. “There is no other organization that combines community, product, technology and advocacy to produce trusted innovative products that people love. I’ve always admired Mozilla for its principled, people-focused approach.</blockquote>'
+};
