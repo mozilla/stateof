17bbff630a5264d3bcbe0bd8e628784eb2f55144
wuebkers <skylar@skylarwuebker.com>
2023-11-21 10:35:05 -0500

    adds correct headshots and removes excerpts

--- src/testimonies/00-linda-griffin.ts
+++ src/testimonies/00-linda-griffin.ts
@@ -5,10 +5,10 @@ export const content: object = {
   author: {
     name: 'Linda Griffin',
     job_title: 'VP of Public Policy',
-    image: '/headshots/zeke-howard.png',
+    image: '/headshots/linda-griffin.jpg',
   },
   lead_article: true,
   modal_style: 'short',
-  excerpt: 'Carlos Torres joined the team in August as Chief Legal Officer.',
+  excerpt: null,
   body: '<p>My working life has been driven by two passionate beliefs: that international politics, and tech, have unrivaled potential to be forces for good in the world. In fact, the more that’s put into doubt, the stronger my commitment has been to making it happen.</p><p>Mozilla has been part of the fabric of the internet since its beginning. From the days of Web 1.0 Mozilla has been the heart and soul of our technological revolution. Today, Mozilla’s work spreads well beyond the internet of the last century, bringing with it a diversity of approaches and views from across the Corporation, the Foundation, Mozilla Ventures, and Mozilla.ai.</p><p>Concepts like security and privacy are sometimes mere buzzwords in the broader tech ecosystem. At Mozilla, they’re not just an afterthought. They are at the core of everything we build – from Firefox to our communities. Our aim is not just to develop products that people can trust but to build movements that give voice to the views of those people.</p><p>The mission has always been to keep the internet as an open, global, public resource with opportunities for community and collaboration - for all. And that’s precisely the mission I’ve been looking for.</p><p>The depth and breadth of Mozilla’s work with governments, policymakers, and regulators has been without parallel. However, I am reminded daily that our values and commitments make our policy work uniquely difficult. And it’s getting harder.</p><p>I salute Mozillians past and present who have contributed to this effort. My new role, operating on a pan-Mozilla basis, builds on all of their work. I know that together, we can achieve more than ever for internet users everywhere.</p>'
 };
