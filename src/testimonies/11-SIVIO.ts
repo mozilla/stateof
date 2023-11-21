2d124302131700f592ad7b158bcb110ff4593966
wuebkers <skylar@skylarwuebker.com>
2023-11-21 16:12:40 -0500

    fixes some text

--- src/testimonies/11-SIVIO.ts
+++ src/testimonies/11-SIVIO.ts
@@ -10,5 +10,5 @@ export const content: object = {
   lead_article: true,
   modal_style: 'short',
   excerpt: null,
-  body: '<p>is an independent organization that places citizens at the center of socio-economic and policy change processes. This aim is a part of their broader goal to contribute to Africa’s inclusive socio-economic transformation. Their work includes multi-disciplinary policy research, nurturing citizen agency, and community mobilization to address immediate problems. The organization is committed to creating a citizen-focused democracy and addressing complex challenges in building inclusive societies in Africa.</p>'
+  body: '<p>The SIVIO Institute is an independent organization that places citizens at the center of socio-economic and policy change processes. This aim is a part of their broader goal to contribute to Africa’s inclusive socio-economic transformation. Their work includes multi-disciplinary policy research, nurturing citizen agency, and community mobilization to address immediate problems. The organization is committed to creating a citizen-focused democracy and addressing complex challenges in building inclusive societies in Africa.</p>'
 };
