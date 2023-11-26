37ff1781d38506d3805f123c01b31e6cc8bf6c34
wuebkers <skylar@skylarwuebker.com>
2023-11-26 11:40:37 -0500

    fixes typo

--- src/testimonies/10-Angaza-Vijana.ts
+++ src/testimonies/10-Angaza-Vijana.ts
@@ -10,5 +10,5 @@ export const content: object = {
   lead_article: true,
   modal_style: 'short',
   excerpt: null,
-  body: '<p>Angaza Vijana a non-profit organization in Kenya, is dedicated to the socio-economic empowerment of young students, teenage mothers, and vulnerable youth. Their multifaceted approach includes raising funds for digital literacy projects, educating communities on socio-economic issues, advocating for environmental protection, and offering psychosocial support. Key initiatives include improving access to digital education by enhancing local schools’ digital literacy programs, developing community learning pods to foster a sense of belonging, and creating teen wellness groups for emotional well-being.</p>>'
+  body: '<p>Angaza Vijana a non-profit organization in Kenya, is dedicated to the socio-economic empowerment of young students, teenage mothers, and vulnerable youth. Their multifaceted approach includes raising funds for digital literacy projects, educating communities on socio-economic issues, advocating for environmental protection, and offering psychosocial support. Key initiatives include improving access to digital education by enhancing local schools’ digital literacy programs, developing community learning pods to foster a sense of belonging, and creating teen wellness groups for emotional well-being.</p>'
 };
