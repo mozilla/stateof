96be37469355573932b347d5646489876cd8adbf
wuebkers <skylar@skylarwuebker.com>
2023-11-27 12:09:42 -0500

    adds new testemonies

---
+++ src/testimonies/15-abeba-birhane copy.ts
@@ -0,0 +1,14 @@
+export const content: object = {
+  title: 'NEED TITLE',
+  tag: 'build',
+  hash: '#abeba-birhane',
+  author: {
+    name: 'Dr. Abeba Birhane',
+    job_title: null,
+    image: '/headshots/abeba-birhane.png',
+  },
+  lead_article: true,
+  modal_style: 'short',
+  excerpt: 'These problems are rooted in society and history, and grounded in context. You really have to go back in history to understand why we have such issues permeating datasets and models. But going back in history to investigate is not something people often do.',
+  body: '<p>Abeba Birhane is a cognitive scientist researching human behaviour, social systems, and responsible and ethical Artificial Intelligence (AI). She recently finished her PhD, where she explored the challenges and pitfalls of automating human behaviour through critical examination of existing computational models and audits of large scale datasets. <a href="https://arxiv.org/abs/2206.04179" target="_blank">Read her thesis here.</a></p><p>Abeba is a Senior Fellow in Trustworthy AI at Mozilla Foundation, and an Adjunct Lecturer/Assistant Professor at the School of Computer Science and Statistics at <a href="https://www.tcd.ie/scss/research/" target="_blank">Trinity College Dublin, Ireland.<a/></p>'
+};
---
+++ src/testimonies/18-finn-myrstad.ts
@@ -0,0 +1,14 @@
+export const content: object = {
+  title: 'NEED TITLE',
+  tag: 'build',
+  hash: '#finn-myrstad',
+  author: {
+    name: 'Finn Myrstad',
+    job_title: 'Rise25 honoree & director of digital policy at the Norwegian consumer council (NCC)',
+    image: '/headshots/finn-myrstad.jpg',
+  },
+  lead_article: true,
+  modal_style: 'short',
+  excerpt: 'It was a great honor that made my team and everyone we have worked with on improving digital rights very happy. It is of course also motivating to receive such recognition. Meeting all the other honorees expanded my understanding on all the different things people can do to improve the internet, but also the world.',
+  body: '<p>Finn Lützow-Holm Myrstad is the Director of Digital Policy at the Norwegian Consumer Council (NCC), focusing on national and international issues related to privacy, cyber security, net neutrality and more.</p><p>Using targeted research and global coalition-building, he and his team at NCC convince governments and companies to improve their policies. Finn develops and promotes international standards as EU Chair of the Transatlantic Consumer Dialogue´s Digital Committee (www.tacd.org/about), which gathers 75 organisations from both sides of the Atlantic. Alongside his successful advocacy, Finn shapes opinion through addresses to audiences such as the European Parliament, G20, Harvard, Princeton University, SXSW and TED.</p>'
+};
