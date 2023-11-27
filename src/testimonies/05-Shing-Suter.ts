d5faee73d5ad9368c0ec45054e870e81e5c4dab5
wuebkers <skylar@skylarwuebker.com>
2023-11-26 21:37:09 -0500

    adds subheadings to testemonies

--- src/testimonies/05-Shing-Suter.ts
+++ src/testimonies/05-Shing-Suter.ts
@@ -9,6 +9,6 @@ export const content: object = {
   },
   lead_article: true,
   modal_style: 'medium',
-  excerpt: null,
+  excerpt: 'My role at Mozilla Foundation is to steward our technology stack to amplify our movement-building work.',
   body: '<p>I’ve always had a deep passion for digital equity and believe that the Internet is a transformative tool – it is a potential gateway for endless possibilities and opportunities. In my career in non-profit, I have seen how access to the internet and knowledge of how to maximize utilization of it can unlock doors. At Boys & Girls Clubs of America, I saw the power of the internet firsthand – providing access and connectivity benefitted the entire household, not just the kids who wanted it.</p><p>Knowing how to use the internet and being digitally literate, kept them safe, and helped to provide a knowledge base for how best to leverage the internet.</p><p>My role at Mozilla Foundation is to steward our technology stack to amplify our movement-building work. I joined Mozilla to fight to build a healthier internet. To ensure that technology companies build with privacy by design, and to advocate that emerging technologies are built with long-term impacts in mind. I want to make sure the technology built today is an opportunity for all – and that technology is being designed to benefit people, and not just to create profit.</p>'
 };
