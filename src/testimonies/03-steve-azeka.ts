ed61dc3d07b49919a431317e6664178dc02a409b
wuebkers <skylar@skylarwuebker.com>
2023-11-26 19:39:40 -0500

    makes edits to testemonies headshots

--- src/testimonies/03-steve-azeka.ts
+++ src/testimonies/03-steve-azeka.ts
@@ -8,7 +8,7 @@ export const content: object = {
     image: '/headshots/Steven-Azeka.jpeg',
   },
   lead_article: true,
-  modal_style: 'short',
+  modal_style: 'medium',
   excerpt: null,
   body: '<p>In the past decade, Steven Azeka, who serves as Program Lead for Mozilla’s Responsible Computing Challenge, has been actively involved in educating students from kindergarten to graduate school, on how to use, develop, and critically evaluate technology. His latest career move brought him to Mozilla, where he now leads an initiative aimed at supporting instructors and professors at Higher Education institutions with integrating responsible computing into their curricula.</p><p>What attracted Steve to this role was Mozilla Foundation’s dedication to advancing equity. At Mozilla, technology is not seen as a cure-all solution or as existing in a vacuum; rather, it reflects many of society’s challenges and, at times, exacerbates existing inequalities. The Responsible Computing Challenge offers an opportunity to help address these potential harms by preparing the next generation of technologists to think critically and be aware of the issues that frequently and disproportionately harm the most vunerable communities.</p><p>Steve is excited to see the program grow to new regions and courses over the next couple of years</p>'
 };
